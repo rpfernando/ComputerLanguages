ff49856817289f36d13a95b477c285ccaef4bf3c
Fernando Padilla <rpfernando@hotmail.com>
2013-09-12 16:48:43 -0500

    Update ShuntingYard.py
    We will use snake_case for functions and variables and CamelCase for classes.

--- src/ShuntingYard.py
+++ src/ShuntingYard.py
@@ -10,56 +10,56 @@ class ShuntingYard:
 	# This is the constructor of the class.	
 	def __init__(self, re):
 		#Taking the global necessary values.
-		self.tokenFunction = Global.tokenFunction
-		self.tokenFunctionValue =  Global.tokenFunctionValue
-		self.tokenSeparator =  Global.tokenSeparator
+		self.token_function = Global.token_function
+		self.token_function_value =  Global.token_function_value
+		self.token_separator =  Global.token_separator
 
 		self.re = re
-		self.tokens = self.toTokenList(re)
-		self.rpn = self.toRPN(self.tokens)
+		self.tokens = self.to_token_list(re)
+		self.rpn = self.to_rpn(self.tokens)
 
 	# Metod used to change the string to a list and introduct a concatenation operator.
-	def toTokenList(self, re):
-		TokenList = [i for i in re]
+	def to_token_list(self, re):
+		token_list = [i for i in re]
 
 		# With this for the concatenation operator is introduced.
-		for i in reversed(range(len(TokenList)-1)):
-			if TokenList[i] == self.tokenSeparator['Opening']:
+		for i in reversed(range(len(token_list)-1)):
+			if token_list[i] == self.token_separator['Opening']:
 				continue
-			if TokenList[i+1] == self.tokenSeparator['Ending']:
+			if token_list[i+1] == self.token_separator['Ending']:
 				continue
-			if TokenList[i] in self.tokenFunction.values():
+			if token_list[i] in self.token_function.values():
 				continue
-			if TokenList[i+1] in self.tokenFunction.values():
+			if token_list[i+1] in self.token_function.values():
 				continue
-			TokenList.insert(i+1,self.tokenFunction['Link'])
+			token_list.insert(i+1,self.token_function['Link'])
 
-		return TokenList
+		return token_list
 
 	# This metod convert the prefix notation to postfis notation (rpn).
-	def toRPN(self, tokens):
+	def to_rpn(self, tokens):
 		cola = []
 		pila = []
 		for t in tokens:
 			#This first statement move the operator to the stack and then to the queue.
-			if t in self.tokenFunction.values():
+			if t in self.token_function.values():
 				if len(pila) == 0:
 					pila.append(t)
 				else:
 					auxT = pila.pop()
-					while self.tokenFunctionValue[auxT] < self.tokenFunctionValue[t]:
+					while self.token_function_value[auxT] < self.token_function_value[t]:
 						cola.append(auxT)
 						auxT = pila.pop()
 					pila.append(auxT)
 					pila.append(t)
 			#Here checks that the brackets are consistent.
-			elif t in self.tokenSeparator.values():
-				if t == self.tokenSeparator['Opening']:
+			elif t in self.token_separator.values():
+				if t == self.token_separator['Opening']:
 					pila.append(t)
 				else:
-					if not self.tokenSeparator['Opening'] in pila:
+					if not self.token_separator['Opening'] in pila:
 						raise SyntaxError('Parentesis incompletos')
-					while pila[-1] != self.tokenSeparator['Opening']:
+					while pila[-1] != self.token_separator['Opening']:
 						cola.append(pila.pop())
 					pila.pop()
 			#All the simbols of the alphabet.
@@ -67,7 +67,7 @@ class ShuntingYard:
 				cola.append(t)
 
 		#Checks that the stack is empty.
-		if self.tokenSeparator['Opening'] in pila:
+		if self.token_separator['Opening'] in pila:
 			raise SyntaxError('Parentesis incompletos')
 		while len(pila) != 0:
 			cola.append(pila.pop())
