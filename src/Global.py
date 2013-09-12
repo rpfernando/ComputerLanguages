6ba462dd1bc406ba81bd2efdc00e96d33de7ee93
Fernando Padilla <rpfernando@hotmail.com>
2013-09-12 16:50:08 -0500

    Update Global.py
    We will use snake_case for functions and variables and CamelCase for classes.

--- src/Global.py
+++ src/Global.py
@@ -2,6 +2,6 @@
 	This class will contain the global variables that will be used.
 """
 class Global:
-	tokenFunction = { 'Union':',', 'Link':'#'}
-	tokenFunctionValue =  { ',': 1, '#': 0, '(' : 5}
-	tokenSeparator =  { 'Opening':'(', 'Ending':')'}
+	token_function = { 'Union':',', 'Link':'#'}
+	token_function_value =  { ',': 1, '#': 0, '(' : 5}
+	token_separator =  { 'Opening':'(', 'Ending':')'}
