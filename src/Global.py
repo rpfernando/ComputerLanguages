de23345c37d8711e3e63dfb17e2290b9f07f382c
rpfernando <rpfernando@hotmail.com>
2013-09-19 21:52:23 -0500

    RPN to tree
    Tree is correctly generated

--- src/Global.py
+++ src/Global.py
@@ -3,5 +3,7 @@
 """
 class Global:
 	token_function = { 'Union':',', 'Link':'#'}
-	token_function_value =  { ',': 1, '#': 0, '(' : 5}
+	token_unary_function = {'Closure': '*', 'Positive_Closure': '+'}
+	all_token_funciton = dict(token_function.items() + token_unary_function.items())
+	token_function_value =  {'*': 2, '+': 2, ',': 1, '#': 0, '(' : 5}
 	token_separator =  { 'Opening':'(', 'Ending':')'}
--- src/ShuntingYard.py
+++ src/ShuntingYard.py
@@ -6,76 +6,103 @@
 from Global import *
 
 class ShuntingYard:
-	
 	# This is the constructor of the class.	
 	def __init__(self, re):
-		#Taking the global necessary values.
-		self.token_function = Global.token_function
-		self.token_function_value =  Global.token_function_value
-		self.token_separator =  Global.token_separator
-
 		self.re = re
-		self.tokens = self.to_token_list(re)
-		self.rpn = self.to_rpn(self.tokens)
+		self.tokens = to_token_list(re)
+		self.rpn = to_rpn(self.tokens)
+
+class Node:
+	def __init__(self, char, left=None, rigth=None):
+		self.char = char
+		self.left = left
+		self.rigth = rigth
+
+	def __str__(self):
+		s = ""
+		if self.rigth != None:
+			s += str(self.rigth)
+		if self.left != None:
+			s += str(self.left)
+		s += self.char + ' '
+		return s
+
+def create_tree(rpn):
+	if isinstance(rpn,str):
+		rpn = [i for i in rpn]
+	pila = []
+	for token in rpn:
+		if token in Global.token_function.values():
+			if len(pila) < 2:
+				raise SyntaxError('Faltan operandos')
+			pila.append(Node(token,pila.pop(),pila.pop()))
+		else:
+			pila.append(Node(token))
+	if len(pila) > 1:
+		SyntaxError('Faltan operandos')
+	return pila.pop()
 
 	# Metod used to change the string to a list and introduct a concatenation operator.
-	def to_token_list(self, re):
-		token_list = [i for i in re]
+def to_token_list(re):
+	token_list = [i for i in re]
 
-		# With this for the concatenation operator is introduced.
-		for i in reversed(range(len(token_list)-1)):
-			if token_list[i] == self.token_separator['Opening']:
-				continue
-			if token_list[i+1] == self.token_separator['Ending']:
-				continue
-			if token_list[i] in self.token_function.values():
-				continue
-			if token_list[i+1] in self.token_function.values():
-				continue
-			token_list.insert(i+1,self.token_function['Link'])
+	# With this for the concatenation operator is introduced.
+	for i in reversed(range(len(token_list)-1)):
+		if token_list[i] == Global.token_separator['Opening']:
+			continue
+		if token_list[i+1] == Global.token_separator['Ending']:
+			continue
+		if token_list[i] in Global.token_function.values():
+			continue
+		if token_list[i+1] in Global.token_function.values():
+			continue
+		token_list.insert(i+1,Global.token_function['Link'])
 
-		return token_list
+	return token_list
 
 	# This metod convert the prefix notation to postfis notation (rpn).
-	def to_rpn(self, tokens):
-		cola = []
-		pila = []
-		for t in tokens:
-			#This first statement move the operator to the stack and then to the queue.
-			if t in self.token_function.values():
-				if len(pila) == 0:
-					pila.append(t)
-				else:
-					auxT = pila.pop()
-					while self.token_function_value[auxT] < self.token_function_value[t]:
-						cola.append(auxT)
-						auxT = pila.pop()
-					pila.append(auxT)
-					pila.append(t)
-			#Here checks that the brackets are consistent.
-			elif t in self.token_separator.values():
-				if t == self.token_separator['Opening']:
-					pila.append(t)
-				else:
-					if not self.token_separator['Opening'] in pila:
-						raise SyntaxError('Parentesis incompletos')
-					while pila[-1] != self.token_separator['Opening']:
-						cola.append(pila.pop())
-					pila.pop()
-			#All the simbols of the alphabet.
+def to_rpn(tokens):
+	if isinstance(tokens, str):
+		tokens = to_token_list(tokens)
+	cola = []
+	pila = []
+	tokens.insert(0,Global.token_separator['Opening'])
+	tokens.append(Global.token_separator['Ending'])
+	for t in tokens:
+		#This first statement move the operator to the stack and then to the queue.
+		if t in Global.token_function.values():
+			if len(pila) == 0:
+				pila.append(t)
 			else:
-				cola.append(t)
-
+				auxT = pila.pop()
+				while Global.token_function_value[auxT] < Global.token_function_value[t]:
+					cola.append(auxT)
+					auxT = pila.pop()
+				pila.append(auxT)
+				pila.append(t)
+			#Here checks that the brackets are consistent.
+		elif t in Global.token_separator.values():
+			if t == Global.token_separator['Opening']:
+				pila.append(t)
+			else:
+				if not Global.token_separator['Opening'] in pila:
+					raise SyntaxError('Parentesis incompletos')
+				while pila[-1] != Global.token_separator['Opening']:
+					cola.append(pila.pop())
+				pila.pop()
+		#All the simbols of the alphabet.
+		else:
+			cola.append(t)
 		#Checks that the stack is empty.
-		if self.token_separator['Opening'] in pila:
-			raise SyntaxError('Parentesis incompletos')
-		while len(pila) != 0:
-			cola.append(pila.pop())
+	if Global.token_separator['Opening'] in pila:
+		raise SyntaxError('Parentesis incompletos')
+	while len(pila) != 0:
+		cola.append(pila.pop())
+	return cola
 
-		return cola
 
-
-exp = "(hola, hey) como (estas,te va)"
-sy = ShuntingYard(exp)
-for i in sy.rpn:
-	print i,
+#exp = "(hola, hey) como (estas,te va)"
+exp = "abc,ba,b"
+rpn = to_rpn(exp)
+print rpn
+print create_tree(rpn)
