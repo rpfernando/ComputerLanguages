86ccdd0fedf108339af60904c2ca5ffdccf0f757
rpfernando <rpfernando@hotmail.com>
2013-09-24 19:37:34 -0500

    Table transitions and formatting

--- LenguajesWeb/flask_app.py
+++ LenguajesWeb/flask_app.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, render_template
 from apps.Matches import get_re_matches
+from apps.AFND import create_AFND
+from apps.Global import *
+from apps.Thompson import *
 
 application = app = Flask(__name__)
 app.config["DEBUG"] = True
+
+def get_simbols(states):
+    simbols = []
+    for s in states:
+        for k in s.transitions.keys():
+            if not k in simbols:
+                simbols.append(k)
+    return simbols
+
 
 @app.route('/')
 def index():
@@ -11,14 +23,21 @@ def index():
 @app.route('/error')
 def error():
     e = request.form['error']
-    return render_template('error',e)
+    return render_template('error',e=e)
 
 @app.route('/cosas', methods=['POST'])
 def cosas():
     re = request.form['regex']
     text = request.form['text']
-    found = get_re_matches(re, text)
-    return render_template('tabla.html',found=found)
+    try:
+        found = get_re_matches(re, text)
+        State.id = 0
+        states = create_AFND(re).values()
+        simbols = get_simbols(states)
+        simbols.append(Global.epsilon)
+        return render_template('tabla.html',found=found, simbols=simbols, states=states, re=re)
+    except Exception, e:
+        return render_template('error.html',e=e)
 
 if __name__ == '__main__':
     app.run()
